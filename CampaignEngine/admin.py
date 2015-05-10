8f77ec1f12bf89d57e677fe8487367112fbbd1ad
Malcolm White <malcolmdavidwhite@gmail.com>
2015-05-10 15:39:53 -0500

    Added CampaignEngine models
    Pretty rough and untested

--- CampaignEngine/admin.py
+++ CampaignEngine/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from models import Campaign
+
+
+@admin.register(Campaign)
+class Campaign(admin.ModelAdmin):
+    pass
--- CampaignEngine/models.py
+++ CampaignEngine/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Campaign(models.Model):
+    name = models.TextField
+    created_by = models.ForeignKey(User)
+    created_on = models.DateTimeField
+    accounts_liked = models.ManyToManyField(SocialMediaAccount)
+
+
+class SearchTarget(models.Model):
+    campaigns = models.ManyToManyField(Campaign)
+    services = models.ManyToManyField(SocialMediaService)
+
+    class Meta:
+        abstract = True
+
+
+class TargetPhrase(SearchTarget):
+    phrase = models.TextField
+
+
+class TargetGraph(SearchTarget):
+    central_user = models.TextField
+    depth = models.IntegerField
+
+
+class SocialMediaService(models.Model):
+    name = models.TextField
+    icon = models.ImageField
+    post_name = models.TextField
+
+
+class SocialMediaAccount(models.Model):
+    handle = models.TextField
+    service = models.ForeignKey(SocialMediaService)
+
+
+class FlightDate(models.Model):
+    start = models.DateTimeField
+    end = models.DateTimeField
+    campaign = models.ForeignKey(Campaign)
