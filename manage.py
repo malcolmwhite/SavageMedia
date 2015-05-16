d814ca07201fdabadc023f857ef17863e4bf707d
Malcolm White <malcolmdavidwhite@gmail.com>
2015-05-16 13:18:41 -0500

    Refactored settings
    Now machine / environment specific
    
    secret key set through environment variable. Run syncdb with syncdb
    --settings=SavageMedia.settings.dev (or SavageMedia.settings.prod)

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-import os
 import sys
 
+import os
+
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SavageMedia.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SavageMedia.settings.dev")
 
     from django.core.management import execute_from_command_line
 
